76b5e69aafe642d9065e04517d4c673757efc7c0
Verena <VerenaKasztantowicz@gmail.com>
2018-05-01 18:12:18 +0200

    add clean columns in .csv files

---
+++ import.R
@@ -0,0 +1,6 @@
+# Data
+```{r import}
+atizo <- read_sav("../../Tresors/crowd/data/atizo.sav", user_na = FALSE)
+applause <- read_sav("../../Tresors/crowd/data/applause.sav", user_na = FALSE)
+crowdguru <- read_sav("../../Tresors/crowd/data/crowdguru.sav", user_na = FALSE)
+```
