3ad8ba4ffac652bc9167e684d94d91930a1b6be2
Maximilian Held <info@maxheld.de>
2019-03-02 18:25:47 +0100

    also output to word closes #8

--- render.R
+++ render.R
@@ -4,3 +4,8 @@ rmarkdown::render(
   output_format = rticles::springer_article(), 
   output_dir = "_site"
 )
+rmarkdown::render(
+  input = "crowd_de.Rmd",
+  output_format = bookdown::word_document2(),
+  output_dir = "_site"
+)
