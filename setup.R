bafd329b79646fabae9287ff0fff099deafc3fd1
Maximilian Held <info@maxheld.de>
2019-03-03 22:24:27 +0100

    only set outwidth for latex

--- setup.R
+++ setup.R
@@ -9,6 +9,13 @@ options(digits = 1)  # display only 1 decimal in knitr output
 options(scipen = 999)
 options(knitr.kable.NA = 'Keine Angabe o. Fehlwert')
 
+# image sizes for latex ===
+if (isTRUE(getOption('knitr.in.progress')) && knitr::opts_knit$get("rmarkdown.pandoc.to") == "latex") {
+  knitr::opts_chunk$set(
+    out.width = "1\\linewidth"
+  )
+}
+
 # dependencies ====
 library(tidyverse)
 library(checkmate)
